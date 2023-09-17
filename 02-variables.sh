10b8184c5d5e76156b830938858f94ed297ae3ed
adithyaprakash <adithya@gmail.com>
2023-09-17 14:14:31 +0530

    echo jayanth

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-echo "Hello, this script is executed at times"
+#shell script run the command inside $() and stores the output into variable called DATE.
+DATE=$(date)
+
+echo "Hello, this script is executed at timestamp: $DATE"
