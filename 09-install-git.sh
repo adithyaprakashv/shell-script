ce2bdefbc1951e735909a6cc92629adcaa1432e5
adithyaprakash <adithya@gmail.com>
2023-09-17 19:09:12 +0530

    echo jayanth

--- 09-install-git.sh
+++ 09-install-git.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-echo "hello ap"
+# our program goal is to install mysql
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then 
+    echo "ERROR:: Please run this script with root access"
+#else 
+#    echo "INFO:: You are root user"  
+fi  
+
+yum install git -y
