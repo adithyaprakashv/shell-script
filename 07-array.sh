122c304dc2725980e29c13a4e5352ace3af239d9
adithyaprakash <adithya@gmail.com>
2023-09-17 16:58:45 +0530

    echo jayanth

---
+++ 07-array.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# this is called array, instead of single value it can hold multiple values
+PERSONS=("Guna" "Santhu" "Jayanth")
+# inside array we always refer values with index and it starts from 0
+
+echo "Frist Person: ${PERSONS[0]}"
+
+echo "Frist Person: ${PERSONS[@]}"
