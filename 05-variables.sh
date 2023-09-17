32dcbf15481705d9296ebc18f4603298453b786d
adithyaprakash <adithya@gmail.com>
2023-09-17 15:58:53 +0530

    echo jayanth

---
+++ 05-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# this is the comment, we need to develop a program that can do subtraction of 2 numbers
+
+NUMBER1=$1
+NUMBER2=$2
+# I need to run a command inside shell to sub these 2 numbers
+SUB=$((NUMBER1-NUMBER2))
+
+echo "Addition of 2 numbers is: $SUB"
