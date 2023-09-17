c07f8499aef2f49bca9d18b0a7c35e418966264b
adithyaprakash <adithya@gmail.com>
2023-09-17 16:02:36 +0530

    echo jayanth

--- 05-variables.sh
+++ 05-variables.sh
@@ -7,4 +7,4 @@ NUMBER2=$2
 # I need to run a command inside shell to mul these 2 numbers
 MUL=$((NUMBER1*NUMBER2))
 
-echo "Addition of 2 numbers is: $MUL"
+echo "Multipilication of 2 numbers is: $MUL"
