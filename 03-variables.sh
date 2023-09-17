7d5aba33cdde373f490244bc5a7905de6e1fe060
adithyaprakash <adithya@gmail.com>
2023-09-17 14:23:14 +0530

    echo jayanth

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+ 
+ echo "Hello World, I am learning the shell script"
+ PERSON1=$1 #this is the first value from command line
+ PERSON2=$2 # # this is the second value from command line
+
+ echo "$PERSON1: Hi $PERSON2, Good Night"
+ echo "$PERSON2: Good Night $PERSON1"
+ echo "$PERSON1: Had your dinner"
+ echo "$PERSON2: Not yet dude, did you had $PERSON1"
