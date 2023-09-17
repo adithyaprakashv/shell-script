aab9830709b762b78dff94468cd8032940fb75eb
adithyaprakash <adithya@gmail.com>
2023-09-17 16:01:33 +0530

    echo jayanth

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# this is the comment, we need to develop a program that can do subtraction of 2 numbers
+# this is the comment, we need to develop a program that can do multipilication of 2 numbers
 
 NUMBER1=$1
 NUMBER2=$2
-# I need to run a command inside shell to sub these 2 numbers
-SUB=$((NUMBER1-NUMBER2))
+# I need to run a command inside shell to mul these 2 numbers
+MUL=$((NUMBER1*NUMBER2))
 
-echo "Addition of 2 numbers is: $SUB"
+echo "Addition of 2 numbers is: $MUL"
