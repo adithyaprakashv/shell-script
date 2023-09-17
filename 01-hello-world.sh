70f26d30c1f93737fe2776c1e6cc6d781e58d5aa
adithyaprakash <adithya@gmail.com>
2023-09-17 13:08:25 +0530

    echo jayanth

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -3,6 +3,6 @@
  echo "Hello World, I am learning the shell script"
 
  echo "jayanth: Hi prakash, Good Night"
- echo "prakash: Good Night prakash"
+ echo "prakash: Good Night jayanth"
  echo "jayanth: Had your dinner"
  echo "prakash: Not yet dude"
