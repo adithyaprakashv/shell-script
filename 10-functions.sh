845ca1f8396d6198592a7f6a234d0ab828f4c5b0
adithyaprakash <adithya@gmail.com>
2023-09-19 10:25:35 +0530

    echo jayanth

--- 10-functions.sh
+++ 10-functions.sh
@@ -2,8 +2,12 @@
 
 # our program goal is to install mysql
 
-USERID=$(id -u)
+DATE=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$0
+LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
+
 #this function should validate the previous commond and inform user it is success or failure
+
 VALIDATE(){
     #$1 --> it will receive the argument1
     if [ $1 -ne 0 ]
@@ -15,6 +19,8 @@ VALIDATE(){
     fi
 }
 
+USERID=$(id -u)
+
 if [ $USERID -ne 0 ]
 then 
     echo "ERROR:: Please run this script with root access"
@@ -24,11 +30,11 @@ then
 fi  
 
 # it is our responsibility again to check installation is success or not
-yum install mysql -y
+yum install mysql -y &>>$LOGFILE
 
 VALIDATE $? "Installing MySQL"
 
-yum install postfix -y
+yum install postfix -y &>>$LOGFILE
 
 VALIDATE $? "Installing Postfix"
  
