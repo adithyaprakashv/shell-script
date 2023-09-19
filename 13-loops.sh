162ec407b240eb5f7cbde56cd2a91c67213b625c
adithyaprakash <adithya@gmail.com>
2023-09-19 16:28:43 +0530

    echo jayanth

--- 13-loops.sh
+++ 13-loops.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 DATE=$(date +%F)
-LOGSDIR=C/Users/ssap0/shellscript-logs
-#C/Users/ssap0/shellscript-logs/script-name-date.log
+LOGSDIR=/Users/ssap0/shellscript-logs
+# /Users/ssap0/shellscript-logs/script-name-date.log
 SCRIPT_NAME=$0
 LOGFILE=$LOGSDIR/$0-$DATE.log
 USERID=$(id -u)
