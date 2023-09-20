ac23f32fbf23f723026b638835d95878a6aa92d8
adithyaprakash <adithya@gmail.com>
2023-09-20 15:05:05 +0530

    echo jayanth

--- 13-loops.sh
+++ 13-loops.sh
@@ -32,10 +32,10 @@ VALIDATE(){
 for i in $@
 do
     yum list installed $i &>>$LOGFILE
-    if [ $? is -ne 0 ];
+    if [ $? is -ne 0 ]
     then
         echo "$i is not installed, let's install it"
-        yum install $i -y2 &>>$LOGFILE
+        yum install $i -y &>>$LOGFILE
         VALIDATE $? "$i"
     else
         echo -e "$Y $i is already installed $N"
